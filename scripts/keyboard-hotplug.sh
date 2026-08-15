8763b9a8d5095904ab5ac34ca4cd7029dab8d2a4
Daniil Solovey <sda010394@gmail.com>
2026-08-16 00:06:18 +0600

    keyboard autoconnect with xkbcomp

---
+++ scripts/keyboard-hotplug.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+apply_keyboard() {
+    xkbcomp -w 0 \
+        -I"$HOME/.xkb" \
+        -R"$HOME/.xkb" \
+        "$HOME/.xkbrc" \
+        "$DISPLAY"
+
+    xset r rate 150 170
+}
+
+xinput test-xi2 --root |
+while IFS= read -r line; do
+    case "$line" in
+        *HierarchyChanged*)
+            sleep 1
+            apply_keyboard
+            ;;
+    esac
+done
